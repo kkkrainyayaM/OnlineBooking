7fd75eaa79713a7ff50050351140de2df78ea96e
Krainyaya Maria <rizhik_masha23@mail,ru>
2020-03-16 22:24:43 +0300

    wrapper added

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/content-wrapper/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ContentWrapperComponent } from './components/content-wrapper/content-wrapper.component';
+import { BookingFormComponent } from './components/content-wrapper/booking-form/booking-form.component';
+import { InfContentComponent } from './components/content-wrapper/inf-content/inf-content.component';
+import { ContactContentComponent } from './components/content-wrapper/contact-content/contact-content.component';
+import { PricingComponent } from './components/content-wrapper/pricing/pricing.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    ContentWrapperComponent,
+    BookingFormComponent,
+    InfContentComponent,
+    ContactContentComponent,
+    PricingComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
