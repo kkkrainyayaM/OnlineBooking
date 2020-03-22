29d3c0b9fbc1c53181a4ac63fa577734342273b5
Krainyaya Maria <rizhik_masha23@mail,ru>
2020-03-22 19:53:14 +0300

    login and register 1

---
+++ src/app/components/booking-flight/booking-flight.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookingFlightComponent } from './booking-flight.component';
+
+describe('BookingFlightComponent', () => {
+  let component: BookingFlightComponent;
+  let fixture: ComponentFixture<BookingFlightComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BookingFlightComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookingFlightComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
