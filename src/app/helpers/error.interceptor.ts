424742084c5dd0709d008851e1bd83fb9a72a974
Krainyaya Maria <rizhik_masha23@mail,ru>
2020-04-01 19:23:17 +0300

    helpers added

--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -10,7 +10,7 @@ import {AuthenticationService} from '../services';
 export class ErrorInterceptor implements HttpInterceptor {
   constructor(private authenticationService: AuthenticationService) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+ intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
       if ([401, 403].indexOf(err.status) !== -1) {
         // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
